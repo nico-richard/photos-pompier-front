90d2d477c3a38fa95a8b56428fac7383e4c36ad9
Nicolas Richard <nicolas.richard42@hotmail.fr>
2023-09-16 10:08:49 +0200

    feat(global): passage sur material

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,61 @@
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PhotoList from "./components/PhotoList";
 import AddPhotoForm from "./components/AddPhotoForm";
 import SearchPhotos from "./components/SearchPhotos";
-import "./styles.scss";
+import "./styles.sass";
+import {
+  IconButton,
+  Typography,
+  Button,
+  Link,
+  AppBar,
+  Toolbar,
+  Box,
+  MenuItem,
+  Menu,
+} from "@mui/material";
+import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
+
+const pages = ["Photos", "Ajouter", "Rechercher"];
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <header>
-                    <h1>Gestion de photos de camions de pompier</h1>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Liste des photos</Link>
-                            </li>
-                            <li>
-                                <Link to="/ajouter">Ajouter une photo</Link>
-                            </li>
-                            <li>
-                                <Link to="/rechercher">
-                                    Rechercher des photos
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
-                <Routes>
-                    <Route path="/" Component={PhotoList} />
-                    <Route path="/ajouter" Component={AddPhotoForm} />
-                    <Route path="/rechercher" Component={SearchPhotos} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Gestion des photos</h1>
+          <AppBar>
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <LocalFireDepartmentIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Gestion des photos
+              </Typography>
+              <Menu open={true}>
+                {pages.map((page) => (
+                  <MenuItem key={page}>
+                    <Typography>{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Toolbar>
+          </AppBar>
+        </header>
+        <Routes>
+          <Route path="/photos" Component={PhotoList} />
+          <Route path="/photo/new" Component={AddPhotoForm} />
+          <Route path="/rechercher" Component={SearchPhotos} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
